f2c402812c7747b63b0ca64831119979a6310a05
SOMASEKAR17 <somasekarnaidu79@gmail.com>
2026-01-14 02:01:14 +0530

    redirecting the link to new portfolio and added /old/ to view the old portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,54 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from 'react';
+
 import './index.css'
 import App from './App.jsx'
 import About from './about.jsx'
 import Contact from './contact.jsx'
 import Projects from './projects.jsx'
 
+/* 🔴 Redirect to new website */
+function RedirectToNewSite() {
+  useEffect(() => {
+    window.location.replace('https://somasekar.vercel.app/');
+  }, []);
+
+  return null;
+}
+
+/* 🔁 Redirect old paths */
+function OldRedirectHandler() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.pathname;
+    const temp = path.split('/');
+    const lastSegment = temp.pop();
+    const laastSecondSegment = temp.pop();
+
+    const validRoutes = ["home", "about", "contact", "projects"];
+
+    if (validRoutes.includes(lastSegment) && laastSecondSegment!=="old") {
+      window.location.replace(`/old/${lastSegment}`);
+    }
+  }, [location.pathname]);
+
+  return null;
+}
+
 createRoot(document.getElementById('root')).render(
-    <Router>
+  <Router>
+    <OldRedirectHandler />
+
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/home" element={<App />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/projects" element={<Projects />} />
+      <Route path="/" element={<RedirectToNewSite />} />
+
+      <Route path="/old" element={<App />} />
+      <Route path="/old/home" element={<App />} />
+      <Route path="/old/about" element={<About />} />
+      <Route path="/old/contact" element={<Contact />} />
+      <Route path="/old/projects" element={<Projects />} />
     </Routes>
   </Router>
-)
+);
